3acaa2fa2fd2e5e1493d7b094c677bbf3fdc8486
heraud_b <baptiste.heraud@epitech.eu>
2017-03-11 23:15:27 +0100

    ok

--- website/web/js/parkcar.js
+++ website/web/js/parkcar.js
@@ -1,12 +1,4 @@
-function get3Parkcar() {
-    console.log("refresh");
-    $.get('https://api.tfl.lu/v1/Occupancy/CarPark', function( data, status ) {
-        if (status == 'success')
-          {
-            console.log(data);
-          }
-          setTimeout(get3Parkcar, 5000);
-      });
+
 
 
       $( document ).ready(function() {
