0bf1f0b88e801966f0a0b38cca4eee01326e7414
heraud_b <baptiste.heraud@epitech.eu>
2017-03-11 20:01:45 +0100

    JS

---
+++ website/web/js/map.js
@@ -0,0 +1,46 @@
+var styleArray = [
+    {
+      featureType: 'all',
+      stylers: [
+        { saturation: -80 }
+      ]
+    },{
+      featureType: 'road.arterial',
+      elementType: 'geometry',
+      stylers: [
+        { hue: '#00ffee' },
+        { saturation: 50 }
+      ]
+    },{
+      featureType: 'poi.business',
+      elementType: 'labels',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    }
+  ];
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: new google.maps.LatLng(49.6088233, 6.1163861),
+          styles: styleArray,
+          streetViewControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+
+        var locations = [
+      {lat: 49.6003126, lng: 6.1132984}
+      ];
