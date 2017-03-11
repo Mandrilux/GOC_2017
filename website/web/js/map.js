3f1289d5047358001cd8d551224d6da3f884b3db
stessy mougeot <stessy.mougeot@epitech.eu>
2017-03-11 20:15:48 +0100

    test

--- website/web/js/map.js
+++ website/web/js/map.js
@@ -1,28 +1,16 @@
-var styleArray = [
-    {
-      featureType: 'all',
-      stylers: [
-        { saturation: -80 }
-      ]
-    },{
-      featureType: 'road.arterial',
-      elementType: 'geometry',
-      stylers: [
-        { hue: '#00ffee' },
-        { saturation: 50 }
-      ]
-    },{
-      featureType: 'poi.business',
-      elementType: 'labels',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    }
-  ];
+var styleArray = [{      featureType: 'all',stylers: [{ saturation: -80 } ]},{featureType: 'road.arterial',elementType: 'geometry',stylers: [{ hue: '#00ffee' },{ saturation: 50 }      ]},{featureType: 'poi.business',elementType: 'labels',stylers: [{ visibility: 'off' }]}];
+
+if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(maPosition);
+
+var myPosLat;
+var myPosLng;
+
 function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 13,
-          center: new google.maps.LatLng(49.6088233, 6.1163861),
+          center: new google.maps.LatLng(myPosLat, myPosLng),
+          //center: new google.maps.LatLng(49.6088233, 6.1163861),
           styles: styleArray,
           streetViewControl: false,
           mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -42,20 +30,11 @@ function initMap() {
       }
 
         var locations = [
-      {lat: 49.6003126, lng: 6.1132984}
-      ];
+            {lat: 49.6003126, lng: 6.1132984}
+        ];
 
           function maPosition(position)
           {
- 
- 
- 		var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          console.log(latitude);
-          console.log(longitude);
- 
- }
- 
- if(navigator.geolocation)
-   navigator.geolocation.getCurrentPosition(maPosition);
-
+ 		    myPosLat = position.coords.latitude;
+            myPosLng = position.coords.longitude;
+        }
