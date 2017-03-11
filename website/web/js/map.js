4de46bc72dd5ef3d4bf0abfe73669cd298bc4919
stessy mougeot <stessy.mougeot@epitech.eu>
2017-03-11 20:24:59 +0100

    test

--- website/web/js/map.js
+++ website/web/js/map.js
@@ -1,20 +1,19 @@
 var styleArray = [{      featureType: 'all',stylers: [{ saturation: -80 } ]},{featureType: 'road.arterial',elementType: 'geometry',stylers: [{ hue: '#00ffee' },{ saturation: 50 }      ]},{featureType: 'poi.business',elementType: 'labels',stylers: [{ visibility: 'off' }]}];
 
-if(navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(maPosition);
+var locations = [
+    {lat: 49.6003126, lng: 6.1132984}
+];
 
-var myPosLat;
-var myPosLng;
+if(navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(initMap);
 
 function initMap() {
-    if(navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(maPosition);
-
-        console.log("myPos:" + myPosLat);
+        console.log("myPos:" + position.coords.latitude);
 
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 13,
-          center: new google.maps.LatLng(49.6088233, 6.1163861),
+          center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+         // center: new google.maps.LatLng(49.6088233, 6.1163861),
           styles: styleArray,
           streetViewControl: false,
           mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -31,15 +30,4 @@ function initMap() {
 
         var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-      }
-
-        var locations = [
-            {lat: 49.6003126, lng: 6.1132984}
-        ];
-
-          function maPosition(position)
-          {
-              console.log("function");
- 		    myPosLat = position.coords.latitude;
-            myPosLng = position.coords.longitude;
-        }
+}
