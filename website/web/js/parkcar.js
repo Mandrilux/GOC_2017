a38ff6f787ab07e841ea93bd2d65b84346dbb5d2
heraud_b <baptiste.heraud@epitech.eu>
2017-03-11 23:24:05 +0100

    call

--- website/web/js/parkcar.js
+++ website/web/js/parkcar.js
@@ -1,17 +1,12 @@
 function get3Parkcar() {
     console.log("refresh 3car");
-    Spark.get("https://api.tfl.lu/v1/Occupancy/CarPark", (req, res) -> {
+    $.get("https://api.tfl.lu/v1/Occupancy/CarPark", (req, res) -> {
     res.header("Access-Control-Allow-Origin", "*"); //important, otherwise its not working
     console.log("ok");
       setTimeout(get3Parkcar, 5000);
  });
     }
-
-    Spark.get("/someRestCallToSpark", (req, res) -> {
-        res.header("Access-Control-Allow-Origin", "*"); //important, otherwise its not working
-        return "some text";
-     });
-
+    
       $( document ).ready(function() {
           console.log( "document loaded" );
           get3Parkcar()
